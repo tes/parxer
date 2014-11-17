217098a83383336c7b0af8ce9d48936638fc0ab3
cliftonc <clifton.cunningham@gmail.com>
2014-11-17 11:58:02 +0000

    Further tweaks and tests

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ var _ = require('lodash');
 
 var parxer = function(config, input, next) {
 
-    var state = State.create();
-
     // Defaults
     config.prefix = config.prefix || 'cx-';
+
+    var state = State.create(config);
 
     var parser = new htmlparser.Parser({
         onopentag: function(tagname, attribs) {
@@ -16,38 +16,33 @@ var parxer = function(config, input, next) {
             if(!matched) {
                 state.setCurrentOutput(Core.createTag(tagname, attribs));
             }
-
         },
         onprocessinginstruction: function(name, data) {
             state.setCurrentOutput('<' + data + '>');
         },
         ontext:function(data) {
             if(state.isNextTextDefault()) {
-                state.setNextTextDefault();
+                state.setDefaultOutput(data);
             } else {
                 state.setCurrentOutput(data);
             }
         },
         oncomment: function(data) {
-            state.setCurrentOutput('<!-- ' + data);
+            state.setCurrentOutput('<!--' + data);
         },
         oncommentend: function() {
-            state.setCurrentOutput(' -->');
+            state.setCurrentOutput('-->');
         },
         onclosetag: function(tagname){
-            if(state.isNextTextDefault()) { state.setNextTextDefault(); }
+            if(state.isNextTextDefault()) { state.setNextTextDefault(false); }
             if(state.isSkipClosingTag()) { return state.setSkipClosingTag(false); }
             state.setCurrentOutput('</' + tagname + '>');
         },
         onend: function() {
-             var timeout = config.backend && config.backend.timeout || '5000';
-             state.waitFor(timeout, true, function(err) {
+             state.waitFor(config, true, function(err) {
                 if(err) { return next(err); }
                 Core.processDeferredStack(config, state, function(err) {
-                    state.waitFor(timeout, false, function(err, content) {
-                        if(err) { return next(err); }
-                        next(null, content);
-                    });
+                    state.waitFor(config, false, next);
                 });
              });
         },
--- tests/parxer.test.js
+++ tests/parxer.test.js
@@ -40,7 +40,7 @@ describe("Simple parsing", function() {
   });
 
   it('should parse url attributes', function(done) {
-      var input = "<html><div id='url' cx-url='{{server:name}}'></div></html>";
+      var input = "<html><div id='url' cx-url='{{server:name}}'>I am some default text</div></html>";
       parxer({
         plugins: [
           require('../Plugins').Url(function(fragment, next) { next(null, fragment.attribs['cx-url']) })
@@ -51,6 +51,25 @@ describe("Simple parsing", function() {
       }, input, function(err, data) {
         var $ = cheerio.load(data);
         expect($('#url').text()).to.be('http://www.google.com');
+        done();
+      });
+  });
+
+  it('should parse a mixture of attributes in a bigger document', function(done) {
+      var input = "<html><div id='test' cx-test='{{environment:name}}'></div><div id='url' cx-url='{{server:name}}'>I am some default text</div></html>";
+      parxer({
+        plugins: [
+          require('../Plugins').Test,
+          require('../Plugins').Url(function(fragment, next) { next(null, fragment.attribs['cx-url']) })
+        ],
+        variables: {
+          'server:name':'http://www.google.com',
+          'environment:name':'test'
+        }
+      }, input, function(err, data) {
+        var $ = cheerio.load(data);
+        expect($('#url').text()).to.be('http://www.google.com');
+        expect($('#test').text()).to.be('test');
         done();
       });
   });
@@ -75,6 +94,14 @@ describe("Simple parsing", function() {
       });
   });
 
+  it('should deal with all the usual html features', function(done) {
+      var input = "<!DOCTYPE html><html><!-- hello --><div class='class'>I am some text</div></html>";
+      parxer({}, input, function(err, data) {
+        expect(data).to.be(input);
+        done();
+      });
+  });
+
   it('should allow you to define your own prefix', function(done) {
       var input = "<html><div id='url' data-my-url='{{server:name}}'></div></html>";
       parxer({
@@ -91,6 +118,95 @@ describe("Simple parsing", function() {
         done();
       });
   });
+
+  it('should deal with managing overall timeout', function(done) {
+      var input = "<html><div id='url' cx-url='{{server:name}}'></div></html>";
+      parxer({
+        timeout: 100,
+        plugins: [
+          require('../Plugins').Url(function(fragment, next) { setTimeout(function() { next(null, fragment.attribs['cx-url']) }, 1000); })
+        ],
+        variables: {
+          'server:name':'http://www.google.com'
+        }
+      }, input, function(err, data) {
+        expect(err).to.contain('Timeout exceeded, failed to respond');
+        done();
+      });
+  });
+
+  it('should deal with a service that returns an error', function(done) {
+      var input = "<html><div id='url' cx-url='{{server:name}}'>Default Content</div></html>";
+      parxer({
+        timeout: 100,
+        showErrors: true,
+        plugins: [
+          require('../Plugins').Url(function(fragment, next) { setTimeout(function() { next('Arrghh'); }, 20)} )
+        ],
+        variables: {
+          'server:name':'http://www.google.com'
+        }
+      }, input, function(err, data) {
+        var $ = cheerio.load(data);
+        expect($('#url').text()).to.be('Arrghh');
+        done();
+      });
+  });
+
+  it('should replace outer when specified with url', function(done) {
+      var input = "<html><div id='url'><div cx-replace-outer='true' cx-url='{{server:name}}'>I am some default text</div></div></html>";
+      parxer({
+        plugins: [
+          require('../Plugins').Url(function(fragment, next) { next(null, fragment.attribs['cx-url']) })
+        ],
+        variables: {
+          'server:name':'http://www.google.com'
+        }
+      }, input, function(err, data) {
+        var $ = cheerio.load(data);
+        expect($('#url').text()).to.be('http://www.google.com');
+        done();
+      });
+  });
+
+   it('should replace outer when specified with bundle', function(done) {
+      var input = "<html><div id='bundle'><div cx-replace-outer='true' cx-bundles='top.js'></div></div></html>";
+      parxer({
+        plugins: [
+          require('../Plugins').Url(function(fragment, next) { next(null, fragment.attribs['cx-url']); })
+        ],
+        cdn: {
+          url: 'http://base.url.com/'
+        },
+        environment: 'test',
+        variables: {
+          'server:name':'http://www.google.com'
+        }
+      }, input, function(err, data) {
+        var $ = cheerio.load(data);
+        expect($('#bundle').text()).to.be('http://base.url.com/test/default/html/top.js.html');
+        done();
+      });
+  });
+
+  it('should deal with a service that returns an error but show default text if configured to', function(done) {
+      var input = "<html><div id='url' cx-url='{{server:name}}'>Default Content</div></html>";
+      parxer({
+        timeout: 100,
+        showErrors: false,
+        plugins: [
+          require('../Plugins').Url(function(fragment, next) { setTimeout(function() { next('Arrghh'); }, 20)} )
+        ],
+        variables: {
+          'server:name':'http://www.google.com'
+        }
+      }, input, function(err, data) {
+        var $ = cheerio.load(data);
+        expect($('#url').text()).to.be('Default Content');
+        done();
+      });
+  });
+
 
 });
 
