3fdfb258d8bf3f779eaacef53beacaf144446764
Gabriel Cebrian <gabceb@gmail.com>
2015-05-13 23:18:09 -0700

    Separated urls and bundles - Separated urls and bundles into different plugins - Separated urls and bundles tests - Made very small changes on the bundles logic to make it more readable. More coming later.

--- Plugins.js
+++ Plugins.js
@@ -1,5 +1,6 @@
 module.exports = {
     Url: require('./lib/plugins/url'),
     Test: require('./lib/plugins/test'),
-    If: require('./lib/plugins/if')
+    If: require('./lib/plugins/if'),
+    Bundle: require('./lib/plugins/bundle')
 };
--- tests/url.test.js
+++ tests/url.test.js
@@ -6,7 +6,7 @@ var render = require('..').render;
 var cheerio = require('cheerio');
 var fs = require('fs');
 
-describe("Core html parsing", function() {
+describe("Url parsing", function() {
 
   it('should parse url attributes', function(done) {
       var input = "<html><div id='url' cx-url='{{server:name}}'>I am some default text</div></html>";
@@ -55,71 +55,6 @@ describe("Core html parsing", function() {
         var $ = cheerio.load(data);
         expect($('#url').text()).to.be('http://www.google.com');
         expect($('#test').text()).to.be('test');
-        done();
-      });
-  });
-
-  it('should parse bundle attributes', function(done) {
-      var input = "<html><div id='bundle' cx-bundles='service-name/top.js'></div></html>";
-      parxer({
-        plugins: [
-          require('../Plugins').Url(function(fragment, next) { next(null, fragment.attribs['cx-url']); })
-        ],
-        cdn: {
-          url: 'http://base.url.com/'
-        },
-        environment: 'test',
-        variables: {
-          'static:service-name|top':'50',
-          'server:name':'http://www.google.com'
-        }
-      }, input, function(err, data) {
-        var $ = cheerio.load(data);
-        expect($('#bundle').text()).to.be('http://base.url.com/service-name/50/html/top.js.html');
-        done();
-      });
-  });
-
-  it('should parse bundle attributes and use direct js urls if in minfied mode', function(done) {
-      var input = "<html><div id='bundle' cx-replace-outer='true' cx-bundles='service-name/top.js'></div></html>";
-      parxer({
-        plugins: [
-          require('../Plugins').Url(function(fragment, next) { next(null, fragment.attribs['cx-url']); })
-        ],
-        cdn: {
-          url: 'http://base.url.com/'
-        },
-        minified: true,
-        environment: 'test',
-        variables: {
-          'static:service-name|top':'50',
-          'server:name':'http://www.google.com'
-        }
-      }, input, function(err, data) {
-        var $ = cheerio.load(data);
-        expect($('html script')[0].attribs.src).to.be('http://base.url.com/service-name/50/js/top.js');
-        done();
-      });
-  });
-
-  it('should parse bundle attributes and use direct css urls if in minfied mode', function(done) {
-      var input = "<html><div id='bundle' cx-replace-outer='true' cx-bundles='service-name/top.css'></div></html>";
-      parxer({
-        plugins: [
-          require('../Plugins').Url(function(fragment, next) { next(null, fragment.attribs['cx-url']); })
-        ],
-        cdn: {
-          url: 'http://base.url.com/'
-        },
-        minified: true,
-        environment: 'test',
-        variables: {
-          'static:service-name|top':'50',
-          'server:name':'http://www.google.com'
-        }
-      }, input, function(err, data) {
-        var $ = cheerio.load(data);
-        expect($('html link')[0].attribs.href).to.be('http://base.url.com/service-name/50/css/top.css');
         done();
       });
   });
@@ -188,27 +123,6 @@ describe("Core html parsing", function() {
       }, input, function(err, data) {
         var $ = cheerio.load(data);
         expect($('#url').text()).to.be('http://www.google.com');
-        done();
-      });
-  });
-
-  it('should replace outer when specified with bundle', function(done) {
-      var input = "<html><div id='bundle'><div cx-replace-outer='true' cx-bundles='service-name/top.js'><script id='default'>This is some default script</script></div></div></html>";
-      parxer({
-        plugins: [
-          require('../Plugins').Url(function(fragment, next) { next(null, fragment.attribs['cx-url']); })
-        ],
-        cdn: {
-          url: 'http://base.url.com/'
-        },
-        environment: 'test',
-        variables: {
-          'server:name':'http://www.google.com'
-        }
-      }, input, function(err, data) {
-        var $ = cheerio.load(data);
-        expect($('#bundle').text()).to.be('http://base.url.com/service-name/YOU_SPECIFIED_A_BUNDLE_THAT_ISNT_AVAILABLE_TO_THIS_PAGE/html/top.js.html');
-        expect($('#default').length).to.be(0);
         done();
       });
   });
