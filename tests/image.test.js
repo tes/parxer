cb0d39d830c6a88083af09a08f3db89905382296
Maurizio Lupo <maurizio.lupo@gmail.com>
2017-02-23 17:28:56 +0000

    added client-hint for any resource: image, videos etc.

--- tests/image.test.js
+++ tests/image.test.js
@@ -48,6 +48,69 @@ describe("Image parsing", function() {
       });
   });
 
+  it('should generate client hint', function(done) {
+      var input = "<html id='bundle'><img cx-src='service-name/image.png' cx-client-hint></html>";
+      parxer({
+        plugins: [
+          require('../Plugins').Image(function(fragment, next) { next(null, fragment.attribs['cx-url']); })
+        ],
+        cdn: {
+          url: 'http://base.url.com/'
+        },
+        environment: 'test',
+        variables: {
+          'static:service-name':'50',
+          'server:name':'http://www.google.com'
+        }
+      }, input, function(err, fragmentCount, data) {
+        var $ = cheerio.load(data);
+        expect($('#bundle').html()).to.be('<link rel="preload" as="image" href="http://base.url.com/service-name/50/img/image.png">');
+        done();
+      });
+  });
+
+  it('should parse video attributes and set its src', function(done) {
+      var input = "<html><video id='bundle' cx-src='service-name/video.mp4'></html>";
+      parxer({
+        plugins: [
+          require('../Plugins').Image(function(fragment, next) { next(null, fragment.attribs['cx-url']); })
+        ],
+        cdn: {
+          url: 'http://base.url.com/'
+        },
+        environment: 'test',
+        variables: {
+          'static:service-name':'50',
+          'server:name':'http://www.google.com'
+        }
+      }, input, function(err, fragmentCount, data) {
+        var $ = cheerio.load(data);
+        expect($('#bundle')[0].attribs.src).to.be('http://base.url.com/service-name/50/img/video.mp4');
+        done();
+      });
+  });
+
+  it('should generate client hint (video)', function(done) {
+      var input = "<html id='bundle'><video cx-src='service-name/video.mp4' cx-client-hint></html>";
+      parxer({
+        plugins: [
+          require('../Plugins').Image(function(fragment, next) { next(null, fragment.attribs['cx-url']); })
+        ],
+        cdn: {
+          url: 'http://base.url.com/'
+        },
+        environment: 'test',
+        variables: {
+          'static:service-name':'50',
+          'server:name':'http://www.google.com'
+        }
+      }, input, function(err, fragmentCount, data) {
+        var $ = cheerio.load(data);
+        expect($('#bundle').html()).to.be('<link rel="preload" as="media" href="http://base.url.com/service-name/50/img/video.mp4">');
+        done();
+      });
+  });
+
   it('should return statistics about images', function(done) {
       var input = "<html><img id='bundle' cx-src='service-name/image.png'></html>";
       parxer({
